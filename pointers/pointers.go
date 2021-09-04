f1e964d9e3f15763099a50e6d7a877f0230eff82
Venzel home <venzelseo@gmail.com>
2021-09-04 03:50:58 -0300

    mais estudos sobre ponteiros

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -27,5 +27,33 @@ func Execute(print bool) {
 		for i := range x {
 			fmt.Println(string(x[i]))
 		}
+
+		/* */
+
+		inverter := func(a *int8) {
+			*a = *a * -1
+		}
+
+		value := int8(10)
+
+		inverter(&value)
+
+		fmt.Println(value)
+
+		/* */
+
+		swap := func(a, b *uint8) {
+			aux := uint8(*b)
+			*b = *a
+			*a = aux
+		}
+
+		a := uint8(9)
+		b := uint8(8)
+
+		swap(&a, &b)
+
+		fmt.Println(a)
+		fmt.Println(b)
 	}
 }
